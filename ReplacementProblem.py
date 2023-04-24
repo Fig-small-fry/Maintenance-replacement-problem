47a4f4447b01fa2c5431db27a1c4866f7babe39f
JoelJa835 <joeljani71@gmail.com>
2023-04-24 19:40:46 +0300

    I made a mistake adding c(0,k) in the replace cost since we are computing it inside phi1. Also had the wrong order for 1 and 0.

--- ReplacementProblem.py
+++ ReplacementProblem.py
@@ -15,7 +15,7 @@ def c(x, k):
 
 # Define the time horizon K and the maximum machine age X
 K = 5
-X = 4   
+X = 4  
 T = 10
 
 # Initialize the value function array and the optimal policy array
@@ -31,25 +31,26 @@ while x <= X and k < K:
     print("keep cost:",keep_cost)
 
     # Compute the expected cost of replacing the machine
-    replace_cost = phi1(x, k) + c(0, k) + V[0][0]
-    print("replace cost:",replace_cost)
+    replace_cost = phi1(x, k) + V[0][0]
+    print("replace cost:",abs(replace_cost))
 
-    # Check if the machine age is equal to the maximum age and the current time period is less than or equal to K
+    # Check if the machine age is equal to the maximum age 
     if x == X :
         V[k][x] = replace_cost
         optimal_policy[k] = 1
         x=1
         k+=1
     else:
-        # Choose the action with the lowest expected cost
-        if replace_cost < keep_cost:
+        # Choose the action with the lowest expected cost 
+        if abs(replace_cost) < keep_cost:
             V[k][x] = replace_cost
-            optimal_policy[k] = 0
+            optimal_policy[k] = 1
+            print("here")
         else:
             V[k][x] = keep_cost
-            optimal_policy[k] = 1
+            optimal_policy[k] = 0
         x += 1
-        k +=1
+        k += 1
 
 
 print(optimal_policy)
